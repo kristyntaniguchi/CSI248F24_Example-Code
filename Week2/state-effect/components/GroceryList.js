ab34dbcaff491f6d14d2a9deeacd2c8395f7dee5
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-09-26 12:22:07 -0700

    Week2 Complete

--- Week2/state-effect/components/GroceryList.js
+++ Week2/state-effect/components/GroceryList.js
@@ -1,9 +1,31 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TextInput,
+} from "react-native";
 import React from "react";
 import { useState } from "react";
 export default function GroceryList() {
   //make a grocery list and add it to state
   const [groceries, setGroceries] = useState(["Milk", "Eggs", "Bread"]);
+  const [newItem, setNewItem] = useState("");
+  function itemHandler() {
+    //In order for state to work I have use use the setGroceries function if I
+    //want to change groceries
+    //you cannot just modify a variable tracked by state
+    //groceries.push(newItem);
+    //we want to create a new array with everything from groceries but add
+    //newitem
+    //... Spread operator = ALL OF THE ITEMS IN [] {}
+    const newArray = [...groceries, newItem];
+    console.log(newArray);
+    setGroceries(newArray);
+    //clear the newItem state after adding to array.
+    setNewItem("");
+  }
   function renderGrocery({ item, index }) {
     //return some UI for each item
     console.log(item);
@@ -26,6 +48,14 @@ export default function GroceryList() {
         renderItem={renderGrocery}
         keyExtractor={(item, index) => index}
       />
+      {/* User input section */}
+      <View>
+        <Text>Enter an item to add to list</Text>
+        <TextInput value={newItem} onChangeText={setNewItem}></TextInput>
+        {/* Test the newItem state is working */}
+        {/* <Text>{newItem}</Text> */}
+        <Button onPress={itemHandler} title="Add"></Button>
+      </View>
     </View>
   );
 }

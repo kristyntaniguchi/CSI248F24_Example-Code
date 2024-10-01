53f58be4e567cfba313f1fe4d6e1adb9a7caf6fa
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-01 12:02:54 -0700

    Updated

--- Week3/CarApp/components/CarList.js
+++ Week3/CarApp/components/CarList.js
@@ -1,13 +1,10 @@
 import { StyleSheet, Text, FlatList } from "react-native";
 import React from "react";
+import CarListItem from "./CarListItem";
 
 export default function CarList({ cars }) {
   //return some UI for each item
-  const renderItem = ({ item }) => (
-    <Text>
-      {item.make} {item.model}
-    </Text>
-  );
+  const renderItem = ({ item }) => <CarListItem car={item} />;
   //return a unique identifier
   const keyExtractor = (item) => item.id;
   //check to make sure cars passed
--- Week3/CarApp/components/CarListItem.js
+++ Week3/CarApp/components/CarListItem.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function CarListItem() {
+export default function CarListItem({ car }) {
   return (
     <View>
-      <Text>CarListItem</Text>
+      <Text>{car.make}</Text>
+      <Text>{car.model}</Text>
+      <View>
+        <Text>{car.year}</Text>
+        <Text>{car.price}</Text>
+      </View>
     </View>
   );
 }
